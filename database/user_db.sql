101283f16356e9f8ecf56a1c7afa2390e2f950fe
VMormoris <vmormoris2014@gmail.com>
2022-03-30 22:05:47 +0300

    Fixing some typos and creating new enums

--- database/user_db.sql
+++ database/user_db.sql
@@ -1,8 +1,10 @@
+
 /* Have to DROP TABLE(s) before we DROP DOMAIN(s) & ENUMARATION(S) */
 /* DROP TABLE(s) */
-DROP TABLE signin_log;
-DROP TABLE resets;
-DROP TABLE users;
+DROP TABLE IF EXISTS tokens;
+DROP TABLE IF EXISTS resets;
+DROP TABLE IF EXISTS signin_log;
+DROP TABLE IF EXISTS users;
 
 --- Unsigned int type for 2 byte integer
 DROP DOMAIN IF EXISTS uint2;
@@ -17,22 +19,42 @@ DROP DOMAIN IF EXISTS uint8;
 CREATE DOMAIN uint8 AS INTEGER CHECK(VALUE>=0);
 
 --- Enumaration for different levels of privileges that can be assign to a user
-DROP TYPE IF EXISTS privileges;
-CREATE TYPE privileges AS ENUM
+DROP TYPE IF EXISTS user_privileges;
+CREATE TYPE user_privileges AS ENUM
 (
     'default',---A normal user that just create an acount 
     'mod',--- A user higher level privileges than a normal user
     'admin'---The highest level privileges that one can have
 );
 
+-- Enumaratio for user's state
+DROP TYPE IF EXISTS user_state;
+CREATE TYPE user_state AS ENUM
+(
+	'new',--- User was just created
+	'valid',--- User has validate his email address (and whatever else is needed it)
+	'blocked',--- User has been blocked due to many failed attempts to login
+	'banned'--- User is permantly banned (for some reason)
+);
+
 --- Enumaration for user flag on specific attempt
-DROP TYPE IF EXISTS flag
-CREATE TYPE flag AS ENUM
+DROP TYPE IF EXISTS log_flag;
+CREATE TYPE log_flag AS ENUM
 (
     'not_flagged',--- Not flagged from a user yet
     'dont_know',--- User isn't sure if he attempt it or someone else
     'safe',--- User is sure that the login attempt was from him/her
     'malicious'--- User has flagged the attempt as a malicious attempt that was done from him
+);
+
+--- Enumaration for resets states
+DROP TYPE IF EXISTS resets_state;
+CREATE TYPE resets_state AS ENUM
+(
+	'new_user',--- Reset link is for a new user (link-shouldn't be expire)
+	'forgot_passwd',--- Reset link that was asked by the user because he forgot his/her password
+	'reset_passwd',--- Reset link that was send to user because he had too many failed attempts
+	'expired'--- Link has expired
 );
 
 --- Users' table
@@ -54,9 +76,8 @@ CREATE TABLE users
     *   just for adding an extra layer of security I understand that it might be annoying but I can't do anything else with my resources.
     */
     "attempts" uint2 DEFAULT 0,--- Number of attempts left to enter the password wrong before the account is blocked
-    --- TODO(Vasilis): Maybe changed blocked to state as enum
-    "blocked" BOOLEAN DEFAULT true,--- Whether the user is blocked or not (User start as block until the validate their email)
-    "created" TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP(),--- Time uppon the account was created
+    "state" user_state DEFAULT true,--- Whether the user is blocked or not (User start as block until the validate their email)
+    "created" TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,--- Time uppon the account was created
     PRIMARY KEY("id"),
     UNIQUE("email")
 );
@@ -69,7 +90,7 @@ CREATE TABLE signin_log
 (
     "id" BIGSERIAL,--- Log's ID (only for internal use) 
     "uid" uint8 NOT NULL,--- User's ID
-    "flag" flag NOT NULL,--- Flag that user has assign to the specific record
+    "flag" log_flag NOT NULL,--- Flag that user has assign to the specific record
     "ip" VARCHAR(15) NOT NULL,--- IPv4 that was used during the signin attempt
     "result" BOOLEAN NOT NULL,---  Whether the signin attempt was successful or not
     "timestamp" TIMESTAMP WITHOUT TIME ZONE NOT NULL,--- Timestamp for when the signin try occuried
@@ -85,8 +106,7 @@ CREATE TABLE resets
 (
     "id" BIGSERIAL,--- Record's ID (only for internal use)
     "uid" uint8 NOT NULL,--- User's ID
-    ---TODO(Vasilis): Maybe change "fresh_acc" to enumaration
-    "fresh_acc" BOOLEAN NOT NULL,--- Flag for whether they link is for a new account or not
+    "state" resets_state NOT NULL,--- Flag for whether they link is for a new account or not
     "link" VARCHAR(128) NOT NULL,--- Link that will be append to the url in order to be used by the user to change password or verify it's email address
     ---TODO(Vasilis): Maybe at default the earliest date possible (as an invalid timestamp)
     "expires" TIMESTAMP WITHOUT TIME ZONE NOT NULL,--- Timestamp for when the links expires
@@ -107,4 +127,4 @@ CREATE TABLE tokens
     "expires" TIMESTAMP WITHOUT TIME ZONE NOT NULL,--- Timestamp for when token expires
     PRIMARY KEY("id"),
     FOREIGN KEY("uid") REFERENCES users("id")
-)
+);
